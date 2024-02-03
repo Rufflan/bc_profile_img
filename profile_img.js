46a751e31c9793528d5fcd65289d80e61be38164
Rufflan <87934447+Rufflan@users.noreply.github.com>
2024-02-03 17:25:19 +0900

    revert

--- profile_img.js
+++ profile_img.js
@@ -1,43 +1,51 @@
 var target = document.body;
 
-function OnlineProfileRun() {
-	// Sets the screen controls
-	console.log("test");
-	DrawText(TextGet((InformationSheetSelection.IsPlayer()) ? "EnterDescription" : "ViewDescription").replace("CharacterName", InformationSheetSelection.Name), 910, 105, "Black", "Gray");
+var callback = (mutationList, observer) => {
+  if(mutationList[0].addedNodes.length > 0){
+  	if(mutationList[0].addedNodes[0].localName == "textarea"){
+		let tmp_profile_pic = mutationList[0].addedNodes[0].value.split("profiles pic:");
+		  
+		  if(tmp_profile_pic.length > 1){
+			  let width = mutationList[0].addedNodes[0].style.width.replace("px","");
+			  let height = mutationList[0].addedNodes[0].style.height.replace("px","");
+			  let profile_pic = tmp_profile_pic[1].split("\n")[0];
+			  let tmp = document.createElement("style");
+			  tmp.setAttribute("id","profile_img");
+			  tmp.innerHTML = 'textarea#DescriptionInput{width:'+(eval(width) * 0.75)+'px !important;}';
+			  document.head.append(tmp);
 
-
-	var callback = (mutationList, observer) => {
-	  if(mutationList[0].addedNodes.length > 0){
-		if(mutationList[0].addedNodes[0].localName == "textarea"){
-			let tmp_profile_pic = mutationList[0].addedNodes[0].value.split("profiles pic:");
-			  if(tmp_profile_pic.length > 1){
-				  ElementPositionFix("DescriptionInput", 36, 100, 160, 1790 * 0.8, 750);
-			  }else{
-				  ElementPositionFix("DescriptionInput", 36, 100, 160, 1790, 750);
-			  }
+			  let tmp_img_section = document.createElement("img");
+			  tmp_img_section.setAttribute("id","img_profile");
+			  tmp_img_section.setAttribute("src",profile_pic);
+			  tmp_img_section.setAttribute("style","z-index: 99;position: absolute;left: "+(eval(width) * 0.75 + 20)+"px;top: "+(eval(height) + 20)+"px;width: "+(eval(width) * 0.2)+"px;height: 30%;object-fit: cover;");
+			  document.body.append(tmp_img_section);
+		  }
+	}else if(mutationList[0].addedNodes[0].localName != "img"){
+		if(document.getElementById("profile_img")){
+			document.getElementById("profile_img").remove();
 		}
-	  }
-	};
-
-	var observer = new MutationObserver(callback);
-
-	var config = {
-		attributes: false,
-		childList: true,
-		characterData: true
-	};
-	observer.observe(target, config);
-
-
-	observer.disconnect();
-/*
-	if(data){
-		ElementPositionFix("DescriptionInput", 36, data[0], data[1], data[2], data[3]);
-	}else{
-		
+		if(document.getElementById("img_profile")){
+			document.getElementById("img_profile").remove();
+		}
 	}
-*/
-	if (InformationSheetSelection.IsPlayer()) DrawButton(1720, 60, 90, 90, "", "White", "Icons/Accept.png", TextGet("LeaveSave"));
-	DrawButton(1820, 60, 90, 90, "", "White", ((InformationSheetSelection.IsPlayer()) ? "Icons/Cancel.png" : "Icons/Exit.png"), TextGet((InformationSheetSelection.IsPlayer()) ? "LeaveNoSave" : "Leave"));
+  }else{
+	if(document.getElementById("profile_img")){
+		document.getElementById("profile_img").remove();
+	}
+	if(document.getElementById("img_profile")){
+		document.getElementById("img_profile").remove();
+	}
+  }
+};
 
-}
+var observer = new MutationObserver(callback);
+
+var config = {
+    attributes: false,
+    childList: true,
+    characterData: true
+};
+observer.observe(target, config);
+
+
+//observer.disconnect();
