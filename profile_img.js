e65bebee08619f6caad2d492014ef40a05b7bb22
Rufflan <87934447+Rufflan@users.noreply.github.com>
2024-02-03 17:38:39 +0900

    Update profile_img.js

--- profile_img.js
+++ profile_img.js
@@ -1,10 +1,38 @@
 var target = document.body;
 
+function calc_position(X, Y, W, H) {
+	
+
+	// Different positions based on the width/height ratio
+	let HRatio = document.getElementById("MainCanvas")[0].clientHeight / 1000;
+	let WRatio = document.getElementById("MainCanvas")[0].clientWidth / 2000;
+	Font *= Math.max(HRatio, WRatio);
+	let Top = document.getElementById("MainCanvas")[0].offsetTop + Y * HRatio;
+	let Height = H * HRatio;
+	let Left = document.getElementById("MainCanvas")[0].offsetLeft + X * WRatio;
+	let Width = W * WRatio;
+
+	// Sets the element style
+	let data = {
+		fontSize: Font + "px",
+		fontFamily: CommonGetFontName(),
+		position: "fixed",
+		left: Left + "px",
+		top: Top + "px",
+		width: Width + "px",
+		height: Height + "px",
+		display: "inline"
+	};
+	return data;
+}
+
 var callback = (mutationList, observer) => {
   if(mutationList[0].addedNodes.length > 0){
   	if(mutationList[0].addedNodes[0].localName == "textarea"){
 		let tmp_profile_pic = mutationList[0].addedNodes[0].value.split("profiles pic:");
-		  
+		let calc_data = calc_position(100, 160, 1790, 750);
+		console.log(calc_data);
+		
 		  if(tmp_profile_pic.length > 1){
 			  let width = mutationList[0].addedNodes[0].style.width.replace("px","");
 			  let height = mutationList[0].addedNodes[0].style.height.replace("px","");
